3619856edc77938f9b9625d229b0a5893dedb09c
Fathoni <m.fathoni@mail.com>
2018-08-31 15:42:37 +0700

    fix: pembacaan komponen nilai pada status aktif untuk total

--- get-all-usulan-for-penilaian.sql
+++ get-all-usulan-for-penilaian.sql
@@ -17,7 +17,7 @@ left join (
 	join hibah.tahapan_kegiatan_skim tks on tks.id_tahapan_kegiatan_skim = tk.id_tahapan_kegiatan_skim
 	join hibah.usulan_kegiatan uk on uk.id_usulan_kegiatan = tk.id_usulan_kegiatan
 	join hibah.usulan_pimnas up on up.id_usulan_kegiatan = uk.id_usulan_kegiatan
-	join hibah.komponen_penilaian kp on kp.id_tahapan_kegiatan_skim = tks.id_tahapan_kegiatan_skim and kp.edisi = uk.thn_pelaksanaan_kegiatan
+	join hibah.komponen_penilaian kp on kp.id_tahapan_kegiatan_skim = tks.id_tahapan_kegiatan_skim and kp.kd_sts_aktif = '1'
 	left join hibah.hasil_penilaian hp on hp.id_plotting_reviewer = pr.id_plotting_reviewer and hp.id_komponen_penilaian = kp.id_komponen_penilaian
 	where uk.thn_pelaksanaan_kegiatan = $1
 	group by pr.id_plotting_reviewer
--- get-usulan-for-penilaian.sql
+++ get-usulan-for-penilaian.sql
@@ -15,7 +15,7 @@ cross join (
 	join hibah.transaksi_kegiatan tk on tk.id_transaksi_kegiatan = pr.id_transaksi_kegiatan
 	join hibah.tahapan_kegiatan_skim tks on tks.id_tahapan_kegiatan_skim = tk.id_tahapan_kegiatan_skim
 	join hibah.usulan_kegiatan uk on uk.id_usulan_kegiatan = tk.id_usulan_kegiatan
-	join hibah.komponen_penilaian kp on kp.id_tahapan_kegiatan_skim = tks.id_tahapan_kegiatan_skim and kp.edisi = uk.thn_pelaksanaan_kegiatan
+	join hibah.komponen_penilaian kp on kp.id_tahapan_kegiatan_skim = tks.id_tahapan_kegiatan_skim and kp.kd_sts_aktif = '1'
 	left join hibah.hasil_penilaian hp on hp.id_plotting_reviewer = pr.id_plotting_reviewer and hp.id_komponen_penilaian = kp.id_komponen_penilaian
 	where pr.id_plotting_reviewer = $1
 ) skor_akhir
